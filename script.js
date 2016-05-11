37065f70b0921a8da10627613987187037063636
Ashwin Sinha <ash@ds.gg>
2016-05-11 03:48:42 -0700

    Testing Observer

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 $(document).ready(function()
 {   
-    //Code to be executed.
-    
-    
-    
+    InitializeMutationObserver();    
 });
 
 function InjectWhoDisDiv()
 {
     //Add functionality
+}
+
+function InitializeMutationObserver()
+{
+    //Mutation Observer
+    var target = $('#profile_popup');
+
+    var config = {
+      attributes: true,
+      childList: false,
+      characterData: false
+    };
+
+    var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        console.log(mutation.type);
+      });
+    });
+
+    observer.observe(target, config);
 }
