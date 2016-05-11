de530ead3f5f0e40824d2473b5b0a643aed75819
Ashwin Sinha <ash@ds.gg>
2016-05-11 08:17:55 -0700

    Disabling InitializeMutationObserver()

--- MutationObserver.js
+++ MutationObserver.js
@@ -57,3 +57,23 @@ http://ryanmorr.com/using-mutation-observers-to-watch-for-element-availability/
     win.ready = ready;
             
 })(this);
+
+/*
+Initializes a MutationObserver for the `#profile_popup` element.
+*/
+function InitializeMutationObserver()
+{
+    //Mutation Observer
+    var target = $('#profile_popup')[0]; //A jQuery object is an array-like wrapper around one or more DOM elements. To get a reference to the actual DOM elements (instead of the jQuery object), get the first object.
+
+    var config = {
+      attributes: true,
+      childList: true,
+      characterData: true,
+      subtree: true
+    };
+
+    var observer = new MutationObserver(function(){InjectWhoDisDiv();});
+
+    observer.observe(target, config);
+}
--- script.js
+++ script.js
@@ -1,45 +1,12 @@
 $(document).ready(function()
 {   
-    InitializeMutationObserver();    
+    ready('#profile_popup', function(element)
+    {
+        InjectWhoDisDiv();
+    });  
 });
 
 function InjectWhoDisDiv()
 {
     $('.profile-card').append('<span class="spinner-bigger"></span>');
 }
-
-function InitializeMutationObserver()
-{
-    //Mutation Observer
-    var target = $('#profile_popup')[0]; //A jQuery object is an array-like wrapper around one or more DOM elements. To get a reference to the actual DOM elements (instead of the jQuery object), get the first object.
-
-    var config = {
-      attributes: true,
-      childList: true,
-      characterData: true,
-      subtree: true
-    };
-
-    var observer = new MutationObserver(debounce(InjectWhoDisDiv(), 100));
-
-    observer.observe(target, config);
-}
-
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
