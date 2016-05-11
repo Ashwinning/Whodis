72ae0251502e692ad87abe21f3bf161525ae7aa9
Ashwin Sinha <ash@ds.gg>
2016-05-11 08:00:10 -0700

    Added debounce function.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function()
 
 function InjectWhoDisDiv()
 {
-    //Add functionality
+    $('.profile-card').append('<span class="spinner-bigger"></span>');
 }
 
 function InitializeMutationObserver()
@@ -21,8 +21,29 @@ function InitializeMutationObserver()
 
     var observer = new MutationObserver(function(mutations) 
     {
-        //Call function
+        
+        
+        InjectWhoDisDiv();
     });
 
     observer.observe(target, config);
 }
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
