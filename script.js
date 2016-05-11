6ed6c4ab55a913f044f60a6d5820a2a640b5d02f
Ashwin Sinha <ash@ds.gg>
2016-05-11 04:00:13 -0700

    Mutation Observer is functional.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function InjectWhoDisDiv()
 function InitializeMutationObserver()
 {
     //Mutation Observer
-    var target = $('#profile_popup');
+    var target = $('#profile_popup')[0]; //A jQuery object is an array-like wrapper around one or more DOM elements. To get a reference to the actual DOM elements (instead of the jQuery object), get the first object.
 
     var config = {
       attributes: true,
@@ -19,10 +19,9 @@ function InitializeMutationObserver()
       characterData: false
     };
 
-    var observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-        console.log(mutation.type);
-      });
+    var observer = new MutationObserver(function(mutations) 
+    {
+        //Call function
     });
 
     observer.observe(target, config);
