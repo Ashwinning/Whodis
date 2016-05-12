9ccf6374aed4ee67400c8cc74174586dbcea2961
Ashwin Sinha <ash@ds.gg>
2016-05-11 23:27:13 -0700

    Switched to attribute .attr() to get correct values.

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ $(document).ready(function()
 
 function InjectWhoDisDiv()
 {
-    console.log($('#profile_popup').data());
+    //console.log($('#profile_popup').data());
     //$('.profile-card').append('<span class="spinner-bigger"></span>');
-    $('.profile-card').append($('#profile_popup').data('screenName'));
+    $('.profile-card').append($('#profile_popup').attr('data-screen-name'));
 
 }
 
