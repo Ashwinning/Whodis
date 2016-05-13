971d539b200b8449d26ab0733cc45e63e27a75b9
Ashwin Sinha <ash@ds.gg>
2016-05-13 23:44:31 +0800

    Need to do JSONP with callback

--- script.js
+++ script.js
@@ -7,7 +7,28 @@ function InjectWhoDisDiv()
 {
     //console.log($('#profile_popup').data());
     //$('.profile-card').append('<span class="spinner-bigger"></span>');
-    $('.profile-card').append($('#profile_popup').attr('data-screen-name'));
+    //$('.profile-card').append($('#profile_popup').attr('data-screen-name'));
+    $('.profile-card').append(addNoteForm);
 
 }
+
+$('#add-note-button').click(AddNote());
+
+function AddNote()
+{
+    //ADD JSONP CALLBACK TO REQUEST TO CIRCUMVENT CROSS-ORIGIN SHIT
+    
+    //upload text from box   
+    $.ajax
+    ({
+        type: "POST",
+        url: 'http://api.whodis.xyz/note/add',
+        dataType: 'json',
+        data: JSON.stringify('{"note": "' +  $('#add-note-text-input').val() + '", "user_id" : "' + $('#profile_popup').attr('data-user-id') + '"}')
+    })
+    //clear form
+}
+
+var addNoteForm = '<div style="margin-bottom:3px;"><center><input type="text" id="add-note-text-input"><button id="add-note-button" style="background-color:#4099FF;text:#fff;">Add</button></center></div>';
+
 
