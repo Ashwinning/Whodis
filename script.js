236fe485b57125e671e879c1d03eef9248a950fd
Ashwin Sinha <ash@ds.gg>
2016-12-21 15:43:31 +0530

    Committing ajax stuff for now, will be replaced with Firebase

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 $(document).ready(function()
 {   
     InitializeMutationObserver();
+    
+    //Get active user
+    $.ajax
+    ({
+        type: "GET",
+        url: 'https://api.whodis.xyz/user',
+        error: function(data) 
+        { 
+            console.log(data);
+            //open a new window note:this is a popup so it may be blocked by your browser
+            var newWindow = window.open("", "new window", "width=200, height=100");
+            //write the data to the document of the newWindow
+            newWindow.document.write(data.responseText);     
+        },
+        success: function(data)
+        {
+            console.log(data);
+        }
+    })
+    
 });
 
 function InjectWhoDisDiv()
@@ -9,24 +29,38 @@ function InjectWhoDisDiv()
     //$('.profile-card').append('<span class="spinner-bigger"></span>');
     //$('.profile-card').append($('#profile_popup').attr('data-screen-name'));
     $('.profile-card').append(addNoteForm);
+    
+    $('#add-note-button').click(function(){
+        console.log('Add');
+        //upload text from box   
+        $.ajax
+        ({
+            type: "POST",
+            url: 'https://api.whodis.xyz/note/add',
+            dataType: 'json',
+            data: JSON.stringify('{"note": "' +  $('#add-note-text-input').val() + '", "user_id" : "' + $('#profile_popup').attr('data-user-id') + '"}'),
+            error: function(data) 
+            { 
+                console.log(data);
+                
+                //open a new window note:this is a popup so it may be blocked by your browser
+                var newWindow = window.open("", "new window", "width=200, height=100");
 
+                //write the data to the document of the newWindow
+                newWindow.document.write(data.responseText); 
+                
+            }
+        })
+        
+        //clear form
+    });
 }
 
-$('#add-note-button').click(AddNote());
+
 
 function AddNote()
 {
-    //ADD JSONP CALLBACK TO REQUEST TO CIRCUMVENT CROSS-ORIGIN SHIT
-    
-    //upload text from box   
-    $.ajax
-    ({
-        type: "POST",
-        url: 'http://api.whodis.xyz/note/add',
-        dataType: 'json',
-        data: JSON.stringify('{"note": "' +  $('#add-note-text-input').val() + '", "user_id" : "' + $('#profile_popup').attr('data-user-id') + '"}')
-    })
-    //clear form
+
 }
 
 var addNoteForm = '<div style="margin-bottom:3px;"><center><input type="text" id="add-note-text-input"><button id="add-note-button" style="background-color:#4099FF;text:#fff;">Add</button></center></div>';
