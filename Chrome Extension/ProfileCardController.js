ee53ed053a5978dea8d8032a4b95a5b7bc59e83f
Ashwin Sinha <ash@ds.gg>
2016-12-28 04:41:36 +0530

    A small little CSS value parser =/

--- Chrome Extension/ProfileCardController.js
+++ Chrome Extension/ProfileCardController.js
@@ -10,6 +10,9 @@ var currentCard = {dataUserId: "", dataAssociatedTweetId: ""};
 // The profile-hover element
 var profileCardObject = $('#profile-hover-container');
 
+// Current state of the card
+var cardIsVisible = false;
+
 /*
     Recieves mutations from MutationObserver,
     Since MutationObserver spams events when the atrributes change (anim),
@@ -22,6 +25,33 @@ function ProfileMutationReciever(mutations)
     var dataUserId = profileCardObject.attr('data-user-id');
     var dataAssociatedTweetId = profileCardObject.attr('data-associated-tweet-id');
 
+    var cardStyle = profileCardObject.attr('style');
+    var cardDisplay = ReadDisplay(cardStyle);
+
+    alert(cardDisplay);
+    //if (cardDisplay = "Block")
+}
+
+/*
+    Reads the `display` property from a given css string.
+*/
+function ReadDisplay(css)
+{
+    var propertyToRead = 'display:' //What are we trying to find?
+    var indexOfDisplay = css.indexOf(propertyToRead); //Where does the property start?
+    var indexOfValue = indexOfDisplay + propertyToRead.length; //Where does the value start? (at the end of )
+    var indexOfNextSemiColon =  css.indexOf(';', indexOfValue); //Value ends at the semicolon (exclusive) Second arg is start index.
+    var value = css.substring(indexOfValue, indexOfNextSemiColon); //Ze value.
+
+    return value.trim();
+
+}
+
+/*
+// OLD code from the ProfileMutationReciever
+// Was chacking for a change in cards
+// This approach doesn't work because twitter erases all content inside the card.
+
     if (currentCard.dataUserId != dataUserId || currentCard.dataAssociatedTweetId != dataAssociatedTweetId)
     {
         //This is not the same card
@@ -31,4 +61,4 @@ function ProfileMutationReciever(mutations)
         //Send card data to App's lifecycle
         OnProfileChange(profileCardObject, dataUserId, dataAssociatedTweetId, mutations);
     }
-}
+*/
