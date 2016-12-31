2bab039b37bbf86ed345f08448f85c0e64a08c10
Ashwin Sinha <ash@ds.gg>
2016-12-31 14:52:58 +0530

    Integrating update function

--- Chrome Extension/Scripts/InPlaceEditController.js
+++ Chrome Extension/Scripts/InPlaceEditController.js
@@ -37,12 +37,19 @@ $.fn.inlineEdit = function(replaceWith, connectWith, twitterID)
     $(this).click(function()
     {
         var elem = $(this);
-
+        var updateFunction = false; // Will this trigger an update or set operation?
         elem.hide();
         elem.after(replaceWith);
         if (elem.text() != emptyNotePrompt)
         {
             $('.whodis-textarea').val(elem.text());
+            // It wasn't an emptyNotePrompt, the update function will be triggered.
+            updateFunction = true;
+        }
+        else
+        {
+            // It was an emptyNotePrompt, the set function will be triggered.
+            updateFunction = false; // Unneccesary, but written for clarity.
         }
         replaceWith.focus();
         autosize(document.querySelectorAll('.whodis-textarea')); //Make textarea autosizable
@@ -52,10 +59,19 @@ $.fn.inlineEdit = function(replaceWith, connectWith, twitterID)
             {
                 //connectWith.val($(this).val()).change();
                 elem.text($(this).val());
-                
-                //Set note in database
-                SetNote(twitterID, {note: elem.text()});
-                //console.log("Note for " + twitterID + " was set to " + elem.text());
+
+                if (updateFunction)
+                {
+                    //Set note in database
+                    UpdateNote(twitterID, {note: elem.text()});
+                    //console.log("Note for " + twitterID + " was set to " + elem.text());
+                }
+                else
+                {
+                    //Set note in database
+                    SetNote(twitterID, {note: elem.text()});
+                    //console.log("Note for " + twitterID + " was set to " + elem.text());
+                }
             }
             $('.whodis-textarea').val(''); //Clear textarea
             $(this).remove();
